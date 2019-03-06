dad9a881313f7ea128c2b20ede43eb901ea2a762
Niko <niko.jernejsek@learningport.com.my>
2019-03-06 13:54:52 +0800

    Modified for Docker container enviroment
    - dockerfile
    - creates db if doesnt exists
    - using os env vars for db conn details

--- flask/notejam/__init__.py
+++ flask/notejam/__init__.py
@@ -1,12 +1,26 @@
+import os
 from flask import Flask
+from sqlalchemy_utils import database_exists, create_database
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
 
+#env vars from system
+dbuser = os.environ['DBUSER']
+dbpass = os.environ['DBPASS']
+dbendpoint = os.environ['DBENDPOINT']
+dburi = 'mysql+pymysql://'+ dbuser +':' + dbpass + '@'+ dbendpoint +'/notejamdb?charset=utf8mb4'
+
 # @TODO use application factory approach
 app = Flask(__name__)
 app.config.from_object('notejam.config.Config')
+app.config['SQLALCHEMY_DATABASE_URI'] = dburi
 db = SQLAlchemy(app)
+
+# create db if doesnt exists & create schema
+if not database_exists(db.engine.url):
+    create_database(db.engine.url)
+
 
 login_manager = LoginManager()
 login_manager.login_view = "signin"
@@ -16,3 +30,6 @@ mail = Mail()
 mail.init_app(app)
 
 from notejam import views
+db.create_all()
+
+
--- flask/notejam/config.py
+++ flask/notejam/config.py
@@ -7,7 +7,7 @@ class Config(object):
     SECRET_KEY = 'notejam-flask-secret-key'
     CSRF_ENABLED = True
     CSRF_SESSION_KEY = 'notejam-flask-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'notejam.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://'+ os.environ['DBUSER'] +':' + os.environ['DBPASS'] + '@' + os.environ['DBENDPOINT'] + '/notejamdb?charset=utf8mb4'
 
 
 class ProductionConfig(Config):
